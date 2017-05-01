a466e17c78bf471cf3bac213fa977c367fb145e5
Jarrod Holliday <jarrodholliday@gmail.com>
2017-05-01 02:44:03 -0700

    Update to build and deploy to my bintray

--- library/src/main/java/com/flipkart/chatheads/ui/ChatHeadManager.java
+++ library/src/main/java/com/flipkart/chatheads/ui/ChatHeadManager.java
@@ -47,7 +47,7 @@ public interface ChatHeadManager<T extends Serializable> {
     /**
      * Should be called when measuring of the container is done.
      * Typically called from onMeasure or onLayout
-     * Only when {@link ChatHeadContainer#getContainerHeight()} && {@link ChatHeadContainer#getContainerWidth()} returns a positive value will arrangements start working
+     * Only when {@link ChatHeadContainer getContainerHeight()} and {@link ChatHeadContainer getContainerWidth()} returns a positive value will arrangements start working
      *
      * @param height
      * @param width
